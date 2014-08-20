9789098f1dba3096de1639c7c975da2a28f175c7
Thom MF <tigredonorte3@gmail.com>
2014-08-20 01:00:32 -0300

    Bug

--- config/urls.php
+++ config/urls.php
@@ -7,9 +7,8 @@ if(!isset($_SERVER['SERVER_NAME'])) $_SERVER['SERVER_NAME'] = "localhost";
 //$project = (PROJECT == "") ? "/": PROJECT;
 define('URL',  "http://".  str_replace(array("//", DS), '/', $_SERVER['SERVER_NAME'] . "/" . PROJECT));
 define('URL_FILES'    , URL . str_replace(array("//", DS), '/', DIR_SUB_DOMAIN . DIR_FILES_RELATIVE."/"));
-define('URL_IMAGENS'  , URL . str_replace(array("//", DS), '/', DIR_FILES_RELATIVE.'/img/'));
+define('URL_IMAGENS'  , URL . "Application/".str_replace(array("//", DS), '/', DIR_FILES_RELATIVE.'/img/'));
 define('URL_RESOURCES', URL . 'recursos/');
 define('URL_JS'       , URL . 'Application/static/js/');
-
 define('URL_CACHE'    , URL_FILES . 'cache/');
 define('URL_LOG'      , URL_FILES . 'log/');
