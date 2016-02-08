2530b860a89dabd622b969e847ebcace9a4824b9
tigredonorte <tigredonorte3@gmail.com>
2016-02-08 08:35:10 -0200

    serverProtocol
    Including server protocol in urls (https if enabled otherwise http)

--- config/urls.php
+++ config/urls.php
@@ -1,11 +1,8 @@
 <?php
 
-//linha adicionada para os testes de unidade (pois não tem esta variavel setada ao executarem)
-if(!isset($_SERVER['SERVER_NAME'])) $_SERVER['SERVER_NAME'] = "localhost";
-
-//urls padroes
-//$project = (PROJECT == "") ? "/": PROJECT;
-define('URL',  "http://".  str_replace(array("//", DS), '/', $_SERVER['SERVER_NAME'] . "/" . PROJECT));
+if(!isset($_SERVER['SERVER_NAME'])) {$_SERVER['SERVER_NAME'] = "localhost";}
+define('URL_PROTOCOL' , (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')|| $_SERVER['SERVER_PORT'] == 443?"https://":'http://');
+define('URL'          , URL_PROTOCOL.  str_replace(array("//", DS), '/', $_SERVER['SERVER_NAME'] . "/" . PROJECT));
 define('URL_FILES'    , URL . "Application/".str_replace(array("//", DS), '/', DIR_SUB_DOMAIN . DIR_FILES_RELATIVE."/"));
 define('URL_IMAGENS'  , URL . "Application/".str_replace(array("//", DS), '/', DIR_SUB_DOMAIN . DIR_FILES_RELATIVE."/img/"));
 define('URL_RESOURCES', URL . 'Application/recursos/');
