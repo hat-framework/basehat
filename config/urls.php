0bba72d760484e3ce1992718bdc01e845d98d6d7
thompson filgueiras <thompson filgueiras>
2017-05-25 08:13:01 -0300

    updated js directories

--- config/urls.php
+++ config/urls.php
@@ -6,6 +6,6 @@ define('URL'          , URL_PROTOCOL.  str_replace(array("//", DS), '/', $_SERVE
 define('URL_FILES'    , URL . "Application/".str_replace(array("//", DS), '/', DIR_SUB_DOMAIN . DIR_FILES_RELATIVE."/"));
 define('URL_IMAGENS'  , URL . "Application/".str_replace(array("//", DS), '/', DIR_SUB_DOMAIN . DIR_FILES_RELATIVE."/img/"));
 define('URL_RESOURCES', URL . 'Application/recursos/');
-define('URL_JS'       , URL . 'Application/static/js/');
+define('URL_JS'       , URL . 'js/');
 define('URL_CACHE'    , URL_FILES . 'cache/');
 define('URL_LOG'      , URL_FILES . 'log/');
